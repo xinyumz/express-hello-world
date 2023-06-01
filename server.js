3a30666ce645efa8a37b37f6f3df6f4b5fc80654
Xinyu Zhang <xinyumz@umich.edu>
2023-06-01 15:32:36 -0400

    test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Successful response.");
+  res.send("Test.");
 });
 
 app.listen(3000, () => console.log("Example app is listening on port 3000."));
