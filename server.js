07e4011b505be8b941d001eb0bf9295a2a36f58b
Xinyu Zhang <xinyumz@umich.edu>
2023-06-01 15:38:12 -0400

    test

--- server.js
+++ server.js
@@ -6,4 +6,9 @@ app.get("/", (req, res) => {
   res.send("Test.");
 });
 
+app.get("/user", (req, res) => {
+  const user = { id: 123, fname: "Andy", lname: "Alexander", animal: "monkey" };
+  res.json("Test.");
+});
+
 app.listen(3000, () => console.log("Example app is listening on port 3000."));
